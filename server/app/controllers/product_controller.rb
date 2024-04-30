af2e757594e2a8213b6f8610ebe3f7badf0e74d1
krystiano13 <crystianotv@gmail.com>
2024-04-30 16:17:13 +0200

    create function for deleting products from database

--- server/app/controllers/product_controller.rb
+++ server/app/controllers/product_controller.rb
@@ -1,48 +1,59 @@
 class ProductController < ApplicationController
     def index
-        products = Product.where(user: params[:user])
+        @products = Product.where(user: params[:user])
 
-        if products.present?
+        if @products.present?
             render json: {
                 products: products
-            }
+            }, status: :ok
         else
             render json: {
                 products: Array.new
-            }
+            }, status: :unprocessable_entity
         end
     end
 
     def create
-        product = Product.new(product_params)
+        @product = Product.new(product_params)
 
-        if product.save!
+        if @product.save!
             render json: {
                 message: "Product created successfully",
-            }
+            }, status: :ok
         else
             render json: {
                 error: "Product could not be created"
-            }
+            }, status: :unprocessable_entity
         end
     end
 
     def update
-        product = Product.find_by(id: params[:id])
+        @product = Product.find_by(id: params[:id])
 
-        if product.present?
-            product.update(product_params)
+        if @product.present?
+            @product.update(product_params)
             render json: {
                 message: "Product updated successfully",
-            }
+            }, status: :ok
         else
             render json: {
                 error: "Product could not be updated"
-            }
+            }, status: :unprocessable_entity
         end
     end
 
     def destroy
+        id = params[:id]
+        
+        if Product.destroy(id)
+            render json: {
+                message: "Product deleted successfully",
+            }, status: :ok
+        else
+            render json: {
+                error: "Product could not be deleted"
+            }, status: :unprocessable_entity
+        end
     end
 
     private 
