a4988e28b55902487a7a3c15fd2d75580a768f2a
krystiano13 <crystianotv@gmail.com>
2024-04-30 19:44:35 +0200

    change accent color

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -6,21 +6,21 @@ export function Navbar() {
       <div className="container flex items-center justify-center p-6 mx-auto text-gray-600 capitalize dark:text-gray-300">
         <NavLink
           to="/"
-          className="text-gray-800 transition-colors duration-300 transform dark:text-gray-200 border-b-2 border-blue-500 mx-1.5 sm:mx-6"
+          className="text-gray-800 transition-colors duration-300 transform dark:text-gray-200 border-b-2 border-red-500 mx-1.5 sm:mx-6"
         >
           home
         </NavLink>
 
         <NavLink
           to="/"
-          className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6"
+          className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform dark:hover:text-gray-200 hover:border-red-500 mx-1.5 sm:mx-6"
         >
           Cart (0.00$)
         </NavLink>
 
         <NavLink
           to="/"
-          className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6"
+          className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform dark:hover:text-gray-200 hover:border-red-500 mx-1.5 sm:mx-6"
         >
           log Out
         </NavLink>
