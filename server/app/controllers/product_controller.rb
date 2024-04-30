a4ca5103f91e23d41608a500826035969aa9fa1e
krystiano13 <crystianotv@gmail.com>
2024-04-30 16:11:12 +0200

    create function for creating new product

--- server/app/controllers/product_controller.rb
+++ server/app/controllers/product_controller.rb
@@ -14,6 +14,21 @@ class ProductController < ApplicationController
     end
 
     def create
-    
+        product = Product.new(product_params)
+
+        if product.save!
+            render json: {
+                message: "Product created successfully",
+            }
+        else
+            render json: {
+                error: "Product could not be created"
+            }
+        end
+    end
+
+    private 
+    def product_params
+        params.require(:product).permit(:name, :price, :user, :product_id, :amount)
     end
 end
