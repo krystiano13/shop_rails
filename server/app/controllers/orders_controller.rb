03eade40e7f94be899a5d6c4d409979dbfd54b6a
krystiano13 <crystianotv@gmail.com>
2024-04-30 19:23:47 +0200

    create routes for order_controller actions

--- server/app/controllers/orders_controller.rb
+++ server/app/controllers/orders_controller.rb
@@ -8,11 +8,11 @@ class OrdersController < ApplicationController
     if @orders.present?
         render json: {
             orders: @orders
-        }
+        }, status: :ok
     else
         render json: {
             orders: Array.new
-        }
+        }, status: :unprocessable_entity
     end
   end
 
@@ -22,28 +22,32 @@ class OrdersController < ApplicationController
     if @order.save
         render json: {
             message: "Order created successfully",
-        }
+        }, status: :ok
     else
         render json: {
             message: "Order not created",
-        }
+        }, status: :unprocessable_entity
     end
   end
 
   def accept
-    @orders = OrderedProduct.find_by(order_id: params[:id])
+    @orders = OrderedProduct.find_by(id: params[:id])
 
     if @orders.present?
         @orders.update(accept: true)
+
+        render json: {
+            message: "Order accepted successfully",
+        }, status: :ok
     else
         render json: {
             error: "Order not found"
-        }
+        }, status: :unprocessable_entity
     end
   end
 
   private
   def order_params
-    params.require(:order).permit(:name, :amount, :order_id, :price, :adress, :postal_code, :person_name, :accept)
+    params.require(:order).permit(:products, :price, :adress, :postal_code, :person_name, :accept)
   end
 end
--- server/config/routes.rb
+++ server/config/routes.rb
@@ -9,10 +9,15 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
+  #products
   get "products/:user", to: "products#index", as: :products
   post "products/add/:user", to: "products#create", as: :add_product
   put "products/edit/:id", to: "products#update", as: :edit_product
   delete "products/delete/:id", to: "products#destroy", as: :delete_product
 
+  #ordered_products
+  get "orders/", to: "orders#index", as: :orders
+  put "orders/accept/:id", to: "orders#accept", as: :orders_accept
+  post "orders/create", to: "orders#create", as: :orders_create
   
 end
