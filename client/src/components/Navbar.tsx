91a6ce0dde33fbc4bb0356f71c62ef21b1acfee3
krystiano13 <crystianotv@gmail.com>
2024-04-30 15:35:07 +0200

    create interface for product card

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -15,7 +15,7 @@ export function Navbar() {
           to="/"
           className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6"
         >
-          Cart
+          Cart (0.00$)
         </NavLink>
 
         <NavLink
--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -1,10 +1,9 @@
-import { Product } from "../components/Product";
+
 
 export function Home() {
   return (
     <>
       <h1>Home</h1>
-      <Product />
     </>
   );
 }
