2b3d3a2997602a4b7000a3c9d781cb7bc90000f3
krystiano13 <crystianotv@gmail.com>
2024-04-30 15:26:36 +0200

    create navbar component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 import { Home } from "./views/Home"
+import { Navbar } from "./components/Navbar";
 
 function App() {
 
   return (
     <main className="w-[100vw] min-h-[100vh]">
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,30 @@
+import { NavLink } from 'react-router-dom';
+
+export function Navbar() {
+  return (
+    <nav className="bg-white shadow dark:bg-gray-800">
+      <div className="container flex items-center justify-center p-6 mx-auto text-gray-600 capitalize dark:text-gray-300">
+        <NavLink
+          to="/"
+          className="text-gray-800 transition-colors duration-300 transform dark:text-gray-200 border-b-2 border-blue-500 mx-1.5 sm:mx-6"
+        >
+          home
+        </NavLink>
+
+        <NavLink
+          to="/"
+          className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6"
+        >
+          Cart
+        </NavLink>
+
+        <NavLink
+          to="/"
+          className="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6"
+        >
+          log Out
+        </NavLink>
+      </div>
+    </nav>
+  );
+}
