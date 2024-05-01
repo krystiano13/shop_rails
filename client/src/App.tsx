55d5f653e5cf17a353b0a8f321b9f38509b4a894
krystiano13 <crystianotv@gmail.com>
2024-05-01 19:50:42 +0200

    create route for 'Cart' view

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { Home } from "./views/Home"
 import { Login } from "./views/Login";
 import { Register } from "./views/Register";
+import { Cart } from "./views/Cart";
 
 import { Navbar } from "./components/Navbar";
 
@@ -18,6 +19,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/cart" element={<Cart />} />
           </Routes>
         </BrowserRouter>
       </main>
--- client/src/components/Product.tsx
+++ client/src/components/Product.tsx
@@ -1,3 +1,6 @@
+import { useContext } from "react";
+import { AuthContext } from "../contexts/AuthContext";
+
 interface Props {
     name: string;
     price: number;
@@ -5,29 +8,32 @@ interface Props {
     description: string;
 }
 
-export const Product:React.FC<Props> = (props) => {
+export const Product: React.FC<Props> = (props) => {
+  const authContext = useContext(AuthContext);
   return (
     <div className="max-w-xs overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800">
       <div className="px-4 py-2">
         <h1 className="text-xl font-bold text-gray-800 uppercase dark:text-white">
-          { props.name }
+          {props.name}
         </h1>
         <p className="mt-1 text-sm text-gray-600 dark:text-gray-400">
-          { props.description }
+          {props.description}
         </p>
       </div>
 
       <img
         className="object-cover w-full h-48 mt-2"
-        src={ props.img }
+        src={props.img}
         alt="product image"
       />
 
       <div className="flex items-center justify-between px-4 py-2 bg-gray-900">
-        <h1 className="text-lg font-bold text-white">${ props.price }</h1>
-        <button className="px-2 py-1 text-xs font-semibold text-gray-900 uppercase transition-colors duration-300 transform bg-white rounded hover:bg-gray-200 focus:bg-gray-400 focus:outline-none">
-          Add to cart
-        </button>
+        <h1 className="text-lg font-bold text-white">${props.price}</h1>
+        {authContext.auth.is_logged_in && (
+          <button className="px-2 py-1 text-xs font-semibold text-gray-900 uppercase transition-colors duration-300 transform bg-white rounded hover:bg-gray-200 focus:bg-gray-400 focus:outline-none">
+            Add to cart
+          </button>
+        )}
       </div>
     </div>
   );
