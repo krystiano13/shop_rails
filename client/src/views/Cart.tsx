c35fdad94f6f9f8eb71ecd5034adc6f00f128eec
krystiano13 <crystianotv@gmail.com>
2024-05-02 13:59:36 +0200

    create summary section in Cart view

--- client/src/views/Cart.tsx
+++ client/src/views/Cart.tsx
@@ -3,7 +3,7 @@ import { CartItem } from "../components/CartItem";
 export function Cart() {
   return (
     <div className="w-[100vw] h-[100vh] pt-24 p-4 overflow-y-auto flex flex-col md:flex-row gap-6 justify-between items-center md:items-start">
-      <table className="block rounded-lg bg-gray-800 max-w-[95vw] md:max-w-[45%]">
+      <table className="block rounded-lg max-h-[80vh] overflow-y-auto bg-gray-800 max-w-[95vw] md:max-w-[45%]">
         <h2 className="p-3 md:p-5 text-white text-2xl">Shopping Cart</h2>
         <tr className="w-full p-3 md:p-5">
           <th className="text-white md:text-lg text-start p-3 md:p-5">Name</th>
@@ -14,7 +14,35 @@ export function Cart() {
           <th className="text-white md:text-lg text-start p-3 md:p-5">Total</th>
         </tr>
         <CartItem />
+        <CartItem />
       </table>
+      <section
+        className="flex flex-col justify-start items-start gap-4 rounded-lg max-h-[80vh] overflow-y-auto bg-gray-800 w-[90vw] md:w-[45%] max-w-[95vw] md:max-w-[45%]"
+        id="summary"
+      >
+        <h2 className="p-3 md:p-5 text-2xl text-white">Summary</h2>
+        <form className="flex flex-col justify-start items-start gap-6 p-3 md:p-5">
+          <input
+            className="p-1 outline-none bg-gray-900 border-b-2 border-b-solid border-b-slate-600 text-white"
+            required
+            placeholder="your name"
+          />
+          <input
+            className="p-1 outline-none bg-gray-900 border-b-2 border-b-solid border-b-slate-600 text-white"
+            required
+            placeholder="address"
+          />
+          <input
+            className="p-1 outline-none bg-gray-900 border-b-2 border-b-solid border-b-slate-600 text-white"
+            required
+            placeholder="postal code"
+          />
+        </form>
+        <p className="text-white p-3 md:p-5 text-xl">Total: $100.00</p>
+        <button className="m-3 p-1 pl-5 pr-5 rounded-lg text-lg md:m-5 text-white bg-slate-700 hover:bg-slate-600 transition-colors">
+          Checkout
+        </button>
+      </section>
     </div>
   );
 }
