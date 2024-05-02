e31ec6e4d5d1453a5f790733bad9f4165dc93c1d
krystiano13 <crystianotv@gmail.com>
2024-05-02 19:17:53 +0200

    create tests for product_controller

--- server/test/controllers/product_controller_test.rb
+++ server/test/controllers/product_controller_test.rb
@@ -1,7 +1,39 @@
 require "test_helper"
 
 class ProductControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get user's products" do
+    @products = Product.where(user: users(:one).email)
+    assert @products.present?
+  end
+
+  test "should create product" do
+    data = {
+      :name => products(:one).name,
+      :price => products(:one).price,
+      :user => products(:one).user,
+      :product_id => products(:one).product_id,
+      :amount => products(:one).amount
+    }
+
+    product = Product.new(data)
+    assert product.save!
+  end
+
+  test "should update product" do
+    data = {
+      :name => products(:one).name,
+      :price => products(:one).price + 2.25,
+      :user => products(:one).user,
+      :product_id => products(:one).product_id,
+      :amount => products(:one).amount + 1
+    }
+
+    product = Product.find(products(:one).id)
+    assert product.update(data)
+  end
+
+  test "should destroy product" do
+    id = products(:two).id
+    assert Product.destroy(id)
+  end
 end
