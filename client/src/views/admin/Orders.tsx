fea7d9677796f19768ce3480dd585306655d390d
krystiano13 <crystianotv@gmail.com>
2024-05-06 15:41:00 +0200

    implement not allowing ordinary user to see Orders view

--- client/src/views/admin/Orders.tsx
+++ client/src/views/admin/Orders.tsx
@@ -1,3 +1,16 @@
+import { useEffect, useContext } from 'react';
+import { AuthContext } from '../../contexts/AuthContext';
+import { useNavigate } from 'react-router-dom';
+
 export function Orders() {
+    const authContext = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(authContext.auth.user !== "admin@admin") {
+            navigate('/');
+        }
+    }, []);
+
     return <></>
 }
