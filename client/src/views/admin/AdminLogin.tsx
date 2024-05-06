d1a3b125a36a4e19f64c6ba3a58330da7daf2408
krystiano13 <crystianotv@gmail.com>
2024-05-06 15:43:40 +0200

    create admin/login view

--- client/src/views/admin/AdminLogin.tsx
+++ client/src/views/admin/AdminLogin.tsx
@@ -1,3 +1,46 @@
+import { NavLink } from "react-router-dom";
+import { useState } from "react";
+
 export function AdminLogin() {
-    return <></>
+    const [errors, setErrors] = useState<string[]>([]);
+    function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        
+    }
+    
+    return (
+      <div className="w-[100vw] h-[100vh] flex justify-center items-center">
+        <div className="w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800">
+          <div className="px-6 py-4">
+            <h3 className="mt-3 text-xl font-medium text-center text-gray-600 dark:text-gray-200">
+              Welcome Admin !
+            </h3>
+
+            <p className="mt-1 text-center text-gray-500 dark:text-gray-400">
+              Login
+            </p>
+
+            <form onSubmit={handleSubmit}>
+              <div className="w-full mt-4">
+                <input
+                  className="block w-full px-4 py-2 mt-2 text-gray-100 placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-red-400 dark:focus:border-red-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-red-300"
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  aria-label="Password"
+                />
+              </div>
+
+              <div className="flex items-center justify-between mt-4">
+                <button className="w-full px-6 py-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-red-500 rounded-lg hover:bg-red-400 focus:outline-none focus:ring focus:ring-red-300 focus:ring-opacity-50">
+                  Sign In
+                </button>
+              </div>
+              {errors.map((item) => (
+                <p className="text-red-500 text-center mt-2">{item}</p>
+              ))}
+            </form>
+          </div>        
+        </div>
+      </div>
+    );
 }
