45c67380d30d702253c72b60a7e8cb83ad028273
krystiano13 <crystianotv@gmail.com>
2024-05-07 12:34:01 +0200

    implement functions that gets data from search params in Order view

--- client/src/utils/products.ts
+++ client/src/utils/products.ts
@@ -7,7 +7,7 @@ import img6 from "../assets/img6.jpg";
 import img7 from "../assets/img7.jpg";
 import img8 from "../assets/img8.jpeg";
 
-type product = {
+export type product = {
     name: string,
     description: string,
     price: number,
--- client/src/views/admin/Order.tsx
+++ client/src/views/admin/Order.tsx
@@ -1,5 +1,36 @@
-export const Order = () => {
-    return (
-        <></>
-    )
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
+import { useNavigate } from "react-router";
+import { useSearchParams } from "react-router-dom";
+import type { product } from "../../utils/products";
+
+export function Order() {
+  const authContext = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const [params, setParams] = useSearchParams();
+  const [products, setProducts] = useState<product[]>([]);
+
+  useEffect(() => {
+    if (!params.get("products")) {
+      navigate("/admin/orders");
+      return;
+    }
+
+    setProducts(
+      () => JSON.parse(params.get("products") as string) as product[]
+    );
+
+    console.log(JSON.parse(params.get("products") as string));
+  }, []);
+
+  useEffect(() => {
+    if (authContext.auth.is_logged_in) {
+      if (authContext.auth.user !== "admin@admin") {
+        navigate("/");
+      }
+    }
+  }, [authContext.auth.is_logged_in]);
+
+  return <></>;
 }
